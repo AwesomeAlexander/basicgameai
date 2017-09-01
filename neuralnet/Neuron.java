5468fc601f184a5a0ec375edc321ebc02a14f717
Alexander <AwesomeAlexander@users.noreply.github.com>
2017-08-31 17:40:47 -0700

    Compute method

--- neuralnet/Neuron.java
+++ neuralnet/Neuron.java
@@ -11,8 +11,9 @@ public class Neuron {
 	 * Default Constructor
 	 * @param previous
 	 */
-	public Neuron(Layer previous[]) {
+	public Neuron(Layer previous) {
 		this.value = Math.random();
+		// TODO Randomize weights array
 	}
 	
 	/**
@@ -24,7 +25,19 @@ public class Neuron {
 		this.value = (Math.random()<0.5) ? parent1.value: parent2.value;
 		// Equivalent to {@code if (Math.random() < 0.5) {this.neurons[i].value = parent1.neurons[i].value} else {this.neurons[i].value = parent2.neurons[i].value}}
 		// ? : is essentially an if...else... statement
+		
+		// TODO Add in weight copying?
 	}
 	
 	// Methods
+	
+	public void compute(Layer previous) {
+		int sum = 0;
+		for (int i=0;i<previous.neurons.length;i++) {
+			sum += previous.neurons[i].value*weights[i];
+		}
+		
+		// Sigmoid Function
+		this.value = 1/(1+Math.pow(Math.E,-sum))
+	}
 }
